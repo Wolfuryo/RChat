2376d2ba4d0b684cb0de8a2f0aef8168ae022d8d
Wolfuryo <claudiu_tirisi898@yahoo.ro>
2017-06-17 10:06:10 +0000

    Update main.js

--- main.js
+++ main.js
@@ -18,10 +18,27 @@ Rchat.internal.vars={};
 Rchat.internal.vars.sending=0;
 Rchat.internal.vars.receiving=0;
 Rchat.internal.vars.mess_per_page=20;
-Rchat.internal.get_data=function(p){
-if(!Rchat.internal.vars.receiving) return;
-$.get("/t"+Rchat.config.topic+((p==0 || p=="l")?"":"p"+p*Rchat.internal.vars.mess_per_page)+"-"+((p=="l")?"?view=newest":""), function(data){
-alert(data);
+Rhcat.internal.vars.oc=[];
+Rchat.internal.get_data=function(){
+if(!Rchat.internal.vars.receiving) return; 
+$.get("/t"+Rchat.config.topic+"-?view=newest", function(data){
+var data_chats=$("#chat_sis", data);
+var len=data_chats.length;
+var i=0;
+if(!Rchat.internal.vars.oc.length || (len && !Rchat.internal.vars.oc.length)){ /*Incarcare initiala sau pagina noua*/
+Rchat.internal.vars.oc=[];
+for(i;i<len;i++){
+$("#chat_content").append(data_chats.eq(i).html());
+Rchat.internal.vars.oc.push(data_chats.eq(i).html());
+};
+} else { /*Mesaje noi, aceeasi pagina*/
+for(i;i<len;i++){
+$("#chat_content").append(data_chats.eq(i).html())
+}
+}
+setTimeout(function(){
+Rchat.internal.get_data();
+}, Rchat.config.delay)
 });
 };
 Rchat.internal.init();
