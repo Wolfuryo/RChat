8b613b5db635432382067efd8cfb50436bee2937
Wolfuryo <claudiu_tirisi898@yahoo.ro>
2017-06-17 09:31:32 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 Rchat.internal={};
+Rchat.config.topic=3;
 Rchat.internal.init=function(){
 $("body").append(Rchat.config.html);
 Rchat.internal.op_cl($("#chat_btn"));
@@ -17,6 +18,7 @@ Rchat.internal.vars.receiving=$("#chat_inner").hasClass("chat_open")?1:0;
 Rchat.internal.vars={};
 Rchat.internal.vars.sending=0;
 Rchat.internal.vars.receiving=0;
+Rchat.internal.vars.mess_per_page=20;
 Rchat.internal.get_data=function(p){
 if(!Rchat.internal.vars.receiving) return;
 $.get("/t"+Rchat.config.topic+((p==0 || p=="l")?"":p*Rchat.internal.vars.mess_per_page)+"-"+((p=="l")?"?view=newest":""), function(data){
